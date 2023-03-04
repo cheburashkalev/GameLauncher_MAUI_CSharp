77cc18c494b64ad8c5c8838af26d2726d405c329
Cheburashka <47310777+cheburashkalev@users.noreply.github.com>
2023-03-04 23:57:59 +0700

    Static is Global!!!!

---
+++ GameLauncher_MAUI_CSharp/Global/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher_MAUI_CSharp.Global
+{
+    public static class Global
+    {
+        public static int MaxNumberOfItems = 100;
+        public static string AppName = "My App";
+    }
+}
