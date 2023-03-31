c6d0375ddc430afbbc75883e6cf57e39cfee2fbb
Wellsaik <1play2fun123@gmail.com>
2023-03-31 14:12:26 +0300

    Launcher app - GetAllDisks()

--- GameLauncher_MAUI_CSharp/Global/LauncherApp.cs
+++ GameLauncher_MAUI_CSharp/Global/LauncherApp.cs
@@ -2,13 +2,14 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text
 using WebSocketSharp.Server;
 using WebSocketSharp;
+using GameLauncher_MAUI_CSharp.Data;
 
 public static class LauncherApp
 {
@@ -17,22 +18,36 @@ public static class LauncherApp
     public static string GITHUB_CLIENT_SECRETS = "c076183289fa6989aec355335e60bfc5bdb4737f";
     public static string redirect_uri = "x-open-laucher://oauth";
     public static string AppPath = AppContext.BaseDirectory + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + ".exe";
+
     public static readonly LiteDatabase db = new LiteDatabase(GetDataBasePath());
+
     public static string GetAppDataDir()
     {
-        if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(
-  Environment.SpecialFolder.ApplicationData), "XOpenLauncher\\")))
+        if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XOpenLauncher\\")))
         {
-            Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(
-  Environment.SpecialFolder.ApplicationData), "XOpenLauncher\\"));
+            Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XOpenLauncher\\"));
         }
-        return Path.Combine(Environment.GetFolderPath(
-  Environment.SpecialFolder.ApplicationData), "XOpenLauncher\\");
+        return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XOpenLauncher\\");
     }
-    public static string GetDataBasePath()
+    public static string GetDataBasePath() =>Path.Combine(GetAppDataDir(), "app.db");
+    
+    public IEnumerable<DiskInfo> GetAllDisks()
     {
-        return Path.Combine(GetAppDataDir(), "app.db");
+        List<DiskInfo> result = new List<DiskInfo>();
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+        foreach (DriveInfo drive in allDrives)
+        {
+            result.Add(new DiskInfo()
+            {
+                Name = drive.VolumeLabel,
+                RootDirectory = drive.RootDirectory.FullName,
+                SizeBytes = drive.TotalSize,
+                AvailableSpaceBytes = drive.AvailableFreeSpace
+            });
+        }
+        return result;
     }
+
     public static int GetAvailablePort(int startingPort)
     {
         IPEndPoint[] endPoints;
@@ -66,6 +81,7 @@ public static class LauncherApp
 
         return 0;
     }
+
     public static void TryConnecntAndSendNewArg(string arg, bool checkOpenedApp)
     {
         RegistryKey rootKey = Registry.CurrentUser;
@@ -85,6 +101,7 @@ public static class LauncherApp
 
         }
     }
+
     public static void StartServer()
     {
         RegistryKey rootKey = Registry.CurrentUser;
